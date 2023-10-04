279596e18aa971c5e006db297f56cac7ef5f5946
Abel Fortunati <abel.fortunati@gmail.com>
2023-10-04 11:26:37 +0200

    card content and details

--- components/index.ts
+++ components/index.ts
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
 import CarCard from './CarCard';
-
+import CarDetails from './CarDetails';
 export {
     Hero, CustomButton, 
     Navbar, 
@@ -15,4 +15,5 @@ export {
     SearchManufacturer,
     CustomFilter,
     CarCard,
+    CarDetails
 }
